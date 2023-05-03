9ee46573fe1b1339c456c69978af9d7a87d781dc
copyNinja1219 <thapacyrus30@gmail.com>
2023-05-04 00:05:48 +0530

    updated height

--- client/src/components/Banks/FundComponent/CreateFund.js
+++ client/src/components/Banks/FundComponent/CreateFund.js
@@ -120,11 +120,12 @@ const CreateFund = () => {
                   id="message"
                   rows="4"
                   name="description"
-                  maxLength={8}
+                  // maxLength={8}
+
                   onChange={(e) =>
                     setFund({ ...Fund, [e.target.name]: e.target.value })
                   }
-                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="block max-h-32 min-h-25 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Write your thoughts here..."
                 ></textarea>
               </div>
